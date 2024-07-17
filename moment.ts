9c61d594a2069188c4ee234f10703b955f981c45
trace-leishenpc <1334241143@qq.com>
2024-07-18 00:32:19 +0800

    增加月份简单

--- moment.ts
+++ moment.ts
@@ -1,4 +1,4 @@
-import { App, Plugin, PluginSettingTab, Setting, Notice, TFile, TFolder } from 'obsidian';
+import {App, Plugin, PluginSettingTab, Setting, Notice, TFile, TFolder} from 'obsidian';
 import {Solar} from 'lunar-typescript';
 import {DateType} from "./enum";
 
@@ -9,15 +9,17 @@ export class Moment {
 	constructor(app: App) {
 		this.app = app;
 	}
+
 	// 执行方法
-	public async execute(dateFormat:string,folderName:string){
+	public async execute(dateFormat: string, folderName: string, titleSize: number = 3) {
+		var solar = Solar.fromDate(new Date());
 		// 1-0 获取文件名 判断是否存在 创建or获取  文件名称格式 提供枚举 做setting参数
-		var fileName = this.genfileName(dateFormat);
-		debugger
-		var filePath = folderName +"/"+fileName + ".md"
+		var fileName = this.genfileName(dateFormat,solar);
+		var filePath = folderName + "/" + fileName + ".md"
 		var file = await this.checkAndCreateFile(filePath);
 
 		// 1-1 获取月份 判断是否存在3级标题 写入or跳过 时间一般是向前的
+
 		// 1-2 获取日期时间
 
 		// 2-0 获取省份城市 对于省份和城市相同的 进行处理（中国 北京） 缺省值 河北 衡水市   高德key做setting参数 缺省值做setting参数
@@ -42,9 +44,22 @@ export class Moment {
 		return file;
 	}
 
-	private genfileName(dateFormat: string): string {
-		var solar = Solar.fromDate(new Date());
+	private genfileName(dateFormat: string,solar:Solar): string {
 		var lunar = solar.getLunar();
+		switch (dateFormat) {
+			case DateType.Lunar:
+				return lunar.getYearInChinese() + lunar.getYearInGanZhi() + lunar.getYearShengXiao() + "年";
+			case DateType.Gregorian:
+				return lunar.getYearInChinese();
+			default:
+				return lunar.getYearInChinese() + lunar.getYearInGanZhi() + lunar.getYearShengXiao() + "年";
+		}
+	}
+
+
+	private genMonth(file: File,solar:Solar): string {
+		var lunar = solar.getLunar();
+		lunar.getCurrentJieQi()
 		switch (dateFormat) {
 			case DateType.Lunar:
 				return lunar.getYearInChinese() + lunar.getYearInGanZhi() + lunar.getYearShengXiao() + "年";
